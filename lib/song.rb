93220d459c3ac16433862cfa9ce740e5939c8d58
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-16 20:19:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,21 @@
+
 class Song
-  
-end 
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def artist_name
+    !!self.artist ? self.artist.name : nil
+  end
+
+end
